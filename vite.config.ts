f6f9f756dade300006b193b99dbb858eb78e537d
FlorianZilliox <50234652+FlorianZilliox@users.noreply.github.com>
2026-06-02 08:49:37 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/git-buddy-e0e620b0'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
